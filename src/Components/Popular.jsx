c2a65826a54ea9b80b68276244f0b5fcb3737366
thasleenaaghil <thasleenalatheefaliyam@gmail.com>
2024-05-08 20:42:10 +0530

    popular updated

--- src/Components/Popular.jsx
+++ src/Components/Popular.jsx
@@ -18,7 +18,7 @@ function Popular() {
     }
   return (
     <>
-    <h2 className='fs-4 m-4 ms-5 ps-2 text-light'><b>Popular</b></h2>
+    <h2 className='fs-4 m-4 ms-md-5 ps-2 text-light'><b>Popular</b></h2>
     <div className='d-flex m-2 ms-5 p-2 ps-4 gap-5 scrollbar-none  ' style={{overflowX:'auto'}}>
        {popular.map((item)=>
        <img src={Image_base_url+item.poster_path} className='w-25 rounded border border-secondary rounded-lg hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer'style={{height:'320px'}} />
