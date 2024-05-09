a60682994746c97190614067a9284fa34c3bf471
thasleenaaghil <thasleenalatheefaliyam@gmail.com>
2024-05-09 11:49:56 +0530

    heightadded

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -19,13 +19,13 @@ function Slider() {
     }
   return (
    
-      <div className= 'slider d-flex w-100 px-1 py-4' >
+      <div className= 'd-flex w-100 px-1 py-4' >
         
        
            
-                  {movieList.map((item,index)=>index<=0&&(  <div><img src={Image_base_url+item.backdrop_path} className='d-flex min-w-full object-cover object-left-top mr-5 rounded-md hover:border-[4px] border-gray-400' style={{height:'460px',position:'absolute'}} />
+                  {movieList.map((item,index)=>index<=0&&(  <div><img src={Image_base_url+item.backdrop_path} className='d-flex min-w-full object-cover object-left-top mr-5 rounded-md hover:border-[4px] border-gray-400' style={{height:'500px',position:'absolute'}} />
                   
-                  <div className='text-light px-16  w-100  mt-5 ' style={{position:'relative',backgroundColor:'transparent',height:'350px',width:'200vh'}} >
+                  <div className='text-light px-16 py-4 w-100  mt-5 ' style={{position:'relative',backgroundColor:'transparent',height:'350px',width:'200vh'}} >
             <h1 style={{backgroundColor:'transparent'}}>{item.title}</h1>
             <p style={{backgroundColor:'transparent',textAlign:'justify',overflowX:'hidden'}}>{item.overview}</p>
             <h4 style={{backgroundColor:'transparent'}}>2h:45min</h4>
